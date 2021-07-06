b167a7cb0972ee66ea020c3b095ce39e88c50ff2
Luis Palacios <lfpalacioss@gmail.com>
2021-07-06 16:41:42 -0500

    Creación de los archivos

---
+++ G_PERSONAL_1.R
@@ -0,0 +1,44 @@
+# ########################################################### #
+# ########  MONITOREO DE LOS AVANCES DE LA BD IGA's  ######## #
+# #########################  BY LF  ######################### #
+# ########################################################### #
+
+
+## ######################## ###
+##     I) SETEO GENERAL     ####
+## ######################## ###
+
+# BORRAR TODO LO ANTERIOR
+rm(list=ls())
+
+# INFO DEL USUARIO
+USER=Sys.info()
+USER=USER[7]
+
+# SETEO DEL WD
+setwd(paste('C:/Users/',USER,'/Google Drive/4) R/2) BD/1) OEFA/2) IGAs',sep = ""))
+
+# CARGANDO ARCHIVO BASE
+source(paste('C:/Users/',USER,'/Google Drive/4) R/1) Scripts/0) SCRIPT BASE/BASE_LF.R',sep = ""))
+
+# CARGAR LA LIBRER?A PARA ORACLE
+# library(RJDBC)
+
+# OTRAS LIB
+library(profvis)
+
+
+###############################
+
+
+
+
+## ######################## ###
+##     I) SETEO GENERAL     ####
+## ######################## ###
+
+# BORRAR TODO LO ANTERIOR
+
+
+
+###############################
